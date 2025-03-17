dc2f1d263110bc177fc84ffe6d2bfe51a3bbb321
AlanR <133004241+alanrlaime@users.noreply.github.com>
2025-03-17 11:29:35 -0400

    Update nose.py

--- nose.py
+++ nose.py
@@ -1,4 +1,4 @@
-num1 = float(input("Ingresa el primer número: "))
-num2 = float(input("Ingresa el segundo número: "))
+num1 = float(input("primer número: "))
+num2 = float(input("segundo número: "))
 suma = num1 + num2
 print("La suma es:", suma)
